ad62f1bc6a398a98e57c9dd97b44433166c0188d
amirali <amirali@DESKTOP-2P0IDPU.localdomain>
2025-02-18 10:54:27 -0500

    First experiment results

---
+++ low-dim.sh
@@ -0,0 +1,16 @@
+python3.12 main.py data/optimize/config/Apache_AllMeasurements.csv | tee results/Apache_AllMeasurements.csv
+python3.12 main.py data/optimize/config/SS-B.csv | tee results/SS-B.csv
+python3.12 main.py data/optimize/config/SS-D.csv | tee results/SS-D.csv
+python3.12 main.py data/optimize/config/SS-E.csv | tee results/SS-E.csv
+python3.12 main.py data/optimize/config/SS-F.csv | tee results/SS-F.csv
+python3.12 main.py data/optimize/config/SS-G.csv | tee results/SS-G.csv
+python3.12 main.py data/optimize/config/SS-H.csv | tee results/SS-H.csv
+python3.12 main.py data/optimize/config/SS-M.csv | tee results/SS-M.csv
+python3.12 main.py data/optimize/config/SS-O.csv | tee results/SS-O.csv
+python3.12 main.py data/optimize/config/wc+rs-3d-c4-obj1.csv | tee results/wc+rs-3d-c4-obj1.csv
+python3.12 main.py data/optimize/config/wc+sol-3d-c4-obj1.csv | tee results/wc+sol-3d-c4-obj1.csv
+python3.12 main.py data/optimize/config/wc+wc-3d-c4-obj1.csv | tee results/wc+wc-3d-c4-obj1.csv
+python3.12 main.py data/optimize/misc/auto93.csv | tee results/auto93.csv
+python3.12 main.py data/optimize/misc/real_estate_valuation.csv | tee results/real_estate_valuation.csv
+python3.12 main.py data/optimize/process/nasa93dem.csv | tee results/nasa93dem.csv
+python3.12 main.py data/optimize/process/pom3d.csv | tee results/pom3d.csv
--- main.py
+++ main.py
@@ -84,7 +84,7 @@ def mape(df, true, pred):
 
 
 
-def cross_val(dataset, repeat=1, folds=5):
+def cross_val(dataset, repeat=5, folds=5):
     for _ in range(repeat):
         dataset = dataset.sample(frac = 1)
         kf = KFold(n_splits=folds, shuffle=False)
@@ -99,20 +99,20 @@ df.drop_duplicates(inplace=True)
 mln=0
 statistics = {}
 for target in [c for c in df.columns if c[-1] in ["+","-"]]:
-    for method in ["asIs", "linear", "lgbm"]:
+    for method in ["knn", "linear", "lgbm", "knn-fs", "linear-fs", "lgbm-fs"]:
         statistics[f"{target}{method}"] = stats.SOME(txt=f"{target}{method}")
 
 for train,test in cross_val(df):    ## 5-Fold Cross Validation with 5 repeats
-    print(mln)
-    mln+=1
+    #print(mln)
+    #mln+=1
     ###########
     ## Stage 1 : Feature Selection (May contain discretization methods)
     ###########
 
     ### Discretization
-    discretizer = KBinsDiscretizer(encode='ordinal', strategy='uniform')  ## Equal Width
+    #discretizer = KBinsDiscretizer(encode='ordinal', strategy='uniform')  ## Equal Width
     discretizer = KBinsDiscretizer(encode='ordinal', strategy='quantile') ## Equal Frequency
-    discretizer = KBinsDiscretizer(encode='ordinal', strategy='kmeans')   ## Kmeans
+    #discretizer = KBinsDiscretizer(encode='ordinal', strategy='kmeans')   ## Kmeans
     num = []
     train_binned = pd.DataFrame()
     for c in train.columns: 
@@ -127,7 +127,7 @@ for train,test in cross_val(df):    ## 5-Fold Cross Validation with 5 repeats
     x_cont  = train.drop(columns=targets)
     for t in targets:
         y = train_binned[t].values
-        #feature_score = [1 for _ in features]              ## No Feature Selection
+        feature_score2 = [1 for _ in features]              ## No Feature Selection
         #feature_score = mutual_info_classif(x_binned, y)    ## Information Gain
         feature_score = reliefF_measure(x_cont.values, y)   ## ReliefF
 
@@ -137,6 +137,10 @@ for train,test in cross_val(df):    ## 5-Fold Cross Validation with 5 repeats
         mean_score, std_dev = np.mean(feature_score), np.std(feature_score)
         threshold = mean_score - 0.7 * std_dev
         final_features = [features[i] for i, score in enumerate(feature_score) if score >= threshold]
+        final_features2 = features
+        
+        feature_selection_diff = False
+        if final_features != final_features2: feature_selection_diff = True
 
     
     ###########
@@ -152,37 +156,69 @@ for train,test in cross_val(df):    ## 5-Fold Cross Validation with 5 repeats
         labeled = [unlabeled.pop() for _ in range(4)]
         budget = math.sqrt(len(unlabeled))
         while (len(labeled) < budget):
-            labeled += [unlabeled.pop(random.randint(0, len(unlabeled)-1))]             ## Random Sampling
-            #labeled += [unlabeled.pop(diversity_sampling(labeled, unlabeled, info))]   ## Diversity Sampling
+            #labeled += [unlabeled.pop(random.randint(0, len(unlabeled)-1))]             ## Random Sampling
+            labeled += [unlabeled.pop(diversity_sampling(labeled, unlabeled, info))]   ## Diversity Sampling
         #labeled = unlabeled                                                             ## No Sampling
+        
+        if feature_selection_diff:
+            unlabeled2 = train[final_features2+[t]].values.tolist() ## Storing training data in a list format including all usefull features and current target
+            info2 = {}
+            info2['head'] = list(train[final_features2].columns)
+            info2['min'] = list(train[final_features2].min())
+            info2['max'] = list(train[final_features2].max())
+            info2['scores'] = feature_score2
+            random.shuffle(unlabeled2)
+            labeled2 = [unlabeled2.pop() for _ in range(4)]
+            budget = math.sqrt(len(unlabeled2))
+            while (len(labeled2) < budget):
+                #labeled += [unlabeled.pop(random.randint(0, len(unlabeled)-1))]             ## Random Sampling
+                labeled2 += [unlabeled2.pop(diversity_sampling(labeled2, unlabeled2, info2))]   ## Diversity Sampling
+            #labeled = unlabeled
+
 
     ###########
     ## Stage 3 : Select Analogy
     ###########
         
-        preds = []
+        preds, preds2 = [], []
         for _,tst in test.iterrows(): 
-            pred_rows = [random.choice(labeled) for _ in range(12)]        ## Random
+            #pred_rows = [random.choice(labeled) for _ in range(12)]        ## Random
             #pred_rows = labeled                                            ## No Analogy
-            #pred_rows, distances = knn(labeled, tst.values.tolist(), 5, info) ## KNN
+            pred_rows, distances = knn(labeled, tst.values.tolist(), 5, info) ## KNN
+            if feature_selection_diff:  pred_rows2, distances2 = knn(labeled2, tst.values.tolist(), 5, info2) ## KNN
     
     ###########
     ## Stage 4 : Make Prediction
     ###########
             cluster = pd.DataFrame(pred_rows, columns=[info['head']+[t]])
-            preds.append(float(cluster[t].mean().iloc[0]))           ## Predicting using mean of cluster
-            #preds.append( weighted_avg(cluster, distances, t) )     ## Predicting using weighted average of cluster
+            #preds.append(float(cluster[t].mean().iloc[0]))           ## Predicting using mean of cluster
+            preds.append( weighted_avg(cluster, distances, t) )     ## Predicting using weighted average of cluster
+            
+            if feature_selection_diff:
+                cluster2 = pd.DataFrame(pred_rows2, columns=[info2['head']+[t]])
+                preds2.append( weighted_avg(cluster2, distances2, t) )   ## Predicting using weighted average of cluster
+
+
     ###########
     ## Stage 5 : Evaluation
     ###########   
-        test[f'{t}asIs'] = preds
+        test[f'{t}knn'] = preds
         test[f'{t}linear'] = regressors.linear(train[final_features],train[t],test[final_features])
         test[f'{t}lgbm'] = regressors.lightgbm(train[final_features],train[t],test[final_features])
+        if feature_selection_diff:
+            test[f'{t}knn-fs'] = preds2
+            test[f'{t}linear-fs'] = regressors.linear(train[final_features2],train[t],test[final_features2])
+            test[f'{t}lgbm-fs'] = regressors.lightgbm(train[final_features2],train[t],test[final_features2])
+        else:
+            test[f'{t}knn-fs'] = test[f'{t}knn']
+            test[f'{t}linear-fs'] = test[f'{t}linear']
+            test[f'{t}lgbm-fs'] = test[f'{t}lgbm']
         
-        for method in ["asIs", "linear", "lgbm"]:
+        for method in ["knn", "linear", "lgbm", "knn-fs", "linear-fs", "lgbm-fs"]:
             statistics[f"{t}{method}"].add(mape(test,t,f'{t}{method}'))
 
 for target in [c for c in df.columns if c[-1] in ["+","-"]]:
+    print(f"\n{target}:")
     stats.report( [vals for st,vals in statistics.items() if target in st] )
 
 ## For train/test :
